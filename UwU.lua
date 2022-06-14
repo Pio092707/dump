c1cc722069f8f27a6d7e15cccdfb16ad2e7f2d9c
Pio092707 <105597710+Pio092707@users.noreply.github.com>
2022-06-15 00:35:18 +0800

    Update UwU.lua

--- UwU.lua
+++ UwU.lua
@@ -1,22 +1,3 @@
-do--//Crash things
-Library = require(game:GetService('ReplicatedStorage').Framework.Library)
-function Death()
-spawn(function()
-Library.Network.Fire('request world', "Fantasy")
-Library.Network.Fire('performed teleport')
-end)
-spawn(function()
-Library.Network.Fire('request world', "Tech")
-Library.Network.Fire('performed teleport')
-end)
-end
-for i=1,15000 do
-spawn(function()
-Death()
-end)
-end
-end
-
 local lib = require(game.ReplicatedStorage.Framework.Library)
 spawn(function()
     for i=1,2300  do
